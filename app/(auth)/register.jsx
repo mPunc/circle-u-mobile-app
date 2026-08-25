8d342cd08ceaa738721329205fddbefb4cac902a
Marko Punčec <mpuncec46@gmail.com>
2026-08-25 22:49:25 +0200

    ThemedScrollView
    Added component and register.jsx visuals pretty much done.
    MP

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,58 +1,77 @@
+import { KeyboardAvoidingView, Platform } from 'react-native'
 
 // themed components
 import ThemedView from '../../components/ThemedView'
-import ThemedText from '../../components/ThemedText'
-import ThemedButton from '../../components/ThemedButton'
+import ThemedScrollView from '../../components/ThemedScrollView'
 import ThemedLogo from '../../components/ThemedLogo'
-import Spacer from '../../components/Spacer'
+import ThemedText from '../../components/ThemedText'
 import InputWithLabel from '../../components/wrappers/InputWithLabel'
+import ThemedButton from '../../components/ThemedButton'
+import Spacer from '../../components/Spacer'
 
 const Register = () => {
   return (
-    <ThemedView className="flex-1 items-center justify-center" safeArea>
-      <ThemedLogo/>
-      <ThemedText variant="subtitle">
-        Welcome to Circle U!
-      </ThemedText>
-      <ThemedText className="text-lg">
-        Please register for an account below
-      </ThemedText>
+    <ThemedView className="flex-1" safeArea>
+      <KeyboardAvoidingView
+        className="flex-1"
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ThemedScrollView
+          className="flex-1"
+          contentContainerClassName="flex-grow items-center justify-center"
+        >
+          <Spacer height={10}/>
+          <ThemedLogo/>
+          <ThemedText variant="subtitle">
+            Welcome to Circle U!
+          </ThemedText>
+          <ThemedText className="text-lg">
+            Please register for an account below
+          </ThemedText>
 
-      <Spacer/>
+          <Spacer/>
 
-      <ThemedView className="flex-initial flex-row w-80 gap-2">
-        <InputWithLabel
-          label="First Name:"
-          className="max-w-80"
+          <ThemedView className="flex-initial flex-row w-80 gap-2">
+            <InputWithLabel
+              label="First Name:"
+              autoCapitalize="words"
+              autoComplete="given-name"
+            />
+            <InputWithLabel
+              label="Last Name:"
+              autoCapitalize="words"
+              autoComplete="family-name"
+            />
+          </ThemedView>
+          <Spacer height={20}/>
+          <InputWithLabel
+            label="Email:"
+            placeholder="email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoComplete="email"
+            autoCorrect={false}
           />
-        <InputWithLabel
-          label="Last Name:"
-          className="max-w-80"
-        />
-      </ThemedView>
-      <Spacer height={20}/>
-      <InputWithLabel
-        label="Email:"
-        className="max-w-80"
-        placeholder="email"
-        keyboardType="email-address"
-      />
-      <Spacer height={20}/>
-      <InputWithLabel
-        label="Password:"
-        className="max-w-80"
-        placeholder="password"
-        secureTextEntry
-      />
+          <Spacer height={20}/>
+          <InputWithLabel
+            label="Password:"
+            placeholder="password"
+            secureTextEntry
+            autoCapitalize="none"
+            autoComplete="new-password"
+          />
 
-      <Spacer height={30}/>
+          <Spacer height={30}/>
 
-      <ThemedButton
-        label="Register"
-        themed={false}
-        className="max-w-80 w-80"
-        onPress={() => console.log("Register button pressed")}
-      />
+          <ThemedButton
+            label="Register"
+            themed={false}
+            className="max-w-80 w-80"
+            onPress={() => console.log("Register button pressed")}
+          />
+          <Spacer height={10}/>
+        </ThemedScrollView>
+      </KeyboardAvoidingView>
     </ThemedView>
   )
 }
--- components/wrappers/InputWithLabel.jsx
+++ components/wrappers/InputWithLabel.jsx
@@ -5,7 +5,7 @@ import ThemedView from "../ThemedView"
 
 const InputWithLabel = ({label = "", ...props}) => {
   return (
-    <ThemedView className="gap-2 w-80 flex-initial items-start justify-center">
+    <ThemedView className="gap-2 w-full max-w-80 flex-initial items-start justify-center">
       <ThemedText className="text-lg">
         {label}
       </ThemedText>
